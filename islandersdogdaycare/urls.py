cfd698306269aefc6f5362a64667b496d2a67707
jjanczynska <joaxjan@gmail.com>
2023-08-29 13:31:43 +0000

    Add paths in urls.py in my apps folder

--- islandersdogdaycare/urls.py
+++ islandersdogdaycare/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 from views import reservations, testimonials
+
+urlpatters = [
+    path('reservations/', views.reservations, name='reservations'),
+    path(
+        'testimonials/<int:testimonial_id>/',
+        views.testimonials, name='testimonials'),
+
+]
