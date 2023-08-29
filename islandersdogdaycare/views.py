d7245aa23dd51da2ccd64cb476106da1972586ad
jjanczynska <joaxjan@gmail.com>
2023-08-29 15:10:32 +0000

    Fixing problems with code so its PEP8 complient

--- islandersdogdaycare/views.py
+++ islandersdogdaycare/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Owner, Dog, Reservation, Testimonial, Comment
-from .forms import OwnerForm, DogForm, ReservationForm, TestimonialForm, CommentForm
+from .forms import (
+    OwnerForm,
+    DogForm,
+    ReservationForm,
+    TestimonialForm,
+    CommentForm
+)
 
 # Reservations View:
 
@@ -14,12 +20,18 @@ def reservations(request):
         dog_form = DogForm(request.POST)
         reservation_form = ReservationForm(request.POST)
 
-        if owner_form.is_valid() and dog_form.is_valid() and reservation_form.is_valid():
+        if (
+            owner_form.is_valid() and
+            dog_form.is_valid() and
+            reservation_form.is_valid()
+        ):
             owner, created = Owner.objects.get_or_create(user=request.user)
             owner.name = owner_form.cleaned_data.get('name')
             owner.tel_no = owner_form.cleaned_data.get('tel_no')
             owner.email_address = owner_form.cleaned_data.get('email_address',)
             owner.save()
+
+            cleaned_data = dog_form.cleaned_data
 
             dog = Dog.objects.create(
                 owner=owner,
@@ -27,7 +39,7 @@ def reservations(request):
                 gender=dog_form.cleaned_data['gender'],
                 breed=dog_form.cleaned_data['breed'],
                 food_provided=dog_form.cleaned_data['food_provided'],
-                vaccinations_up_to_date=dog_form.cleaned_data['vaccinations_up_to_date']
+                vaccinations_up_to_date=cleaned_data['vaccinations_up_to_date']
             )
 
             reservation = reservation_form.save(commit=False)
@@ -50,7 +62,12 @@ def reservations(request):
     return render(
         request,
         'reservations.html',
-        {'owner_form': owner_form, 'dog_form': dog_form, 'reservation_form': reservation_form, 'reservations': existing_reservations}
+        {
+            'owner_form': owner_form,
+            'dog_form': dog_form,
+            'reservation_form': reservation_form,
+            'reservations': existing_reservations
+        }
     )
 
 # Testimonials and comments View
