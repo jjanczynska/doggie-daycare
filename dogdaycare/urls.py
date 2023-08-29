24462c49f0f28459616fbf93584056dc2afa53f3
jjanczynska <joaxjan@gmail.com>
2023-08-29 13:35:56 +0000

    Ass paths into the main urls.py file

--- dogdaycare/urls.py
+++ dogdaycare/urls.py
@@ -19,5 +19,6 @@ from islandersdogdaycare import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reservations/', views.reservations, name='reservations')
+    path('reservations/', views.reservations, name='reservations'),
+    path('testimonials/', views.testimonials, name='testimonials')
 ]
