b34b6a6bb77d1acc78f7ea31cc64ead3d66381db
jjanczynska <joaxjan@gmail.com>
2023-08-30 14:48:34 +0000

    Alter models to accommodate widgets for radio buttons and booking callendar

--- islandersdogdaycare/models.py
+++ islandersdogdaycare/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from datetime import datetime
 from cloudinary.models import CloudinaryField
 
 
 # Owner Model
 class Owner(models.Model):
     user = models.OneToOneField(
-        User, 
+        User,
         on_delete=models.CASCADE,
         primary_key=True)
-    name = models.CharField(max_length=100, default="Default Name")
+    name = models.CharField(max_length=100, default='Please enter your name')
     tel_no = models.CharField(max_length=15)
-    email_address = models.EmailField(default="default@example.com")
+    email_address = models.EmailField(default='Enter your email address')
 
     def __str__(self):
         return self.user.username
@@ -26,6 +26,9 @@ class Dog(models.Model):
         ('F', 'Female'),
     ]
 
+    FOOD_PROVIDED = ((True, 'Yes'), (False, 'No'))
+    VACCINATIONS_UP_TO_DATE = ((True, 'Yes'), (False, 'No'))
+
     owner = models.ForeignKey(
         Owner,
         related_name='dogs',
@@ -37,8 +40,8 @@ class Dog(models.Model):
         null=True,
         blank=True)
     breed = models.CharField(max_length=50)
-    food_provided = models.BooleanField(default=False)
-    vaccinations_up_to_date = models.BooleanField(default=False)
+    food_provided = models.BooleanField(choices=FOOD_PROVIDED)
+    vaccinations_up_to_date = models.BooleanField(choices=VACCINATIONS_UP_TO_DATE)
 
     def __str__(self):
         return self.name
@@ -47,21 +50,21 @@ class Dog(models.Model):
 # Reservation Model
 class Reservation(models.Model):
     owner = models.ForeignKey(
-        Owner, 
-        on_delete=models.CASCADE, 
-        null=True, 
+        Owner,
+        on_delete=models.CASCADE,
+        null=True,
         blank=True)
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
-    date_of_daycare = models.DateField()
+    date_of_daycare = models.DateField(default=datetime.now)
     notes = models.TextField(blank=True)
 
     def __str__(self):
-        return f"Reservation for {self.dog.name} on {self.date_of_daycare}"
+        return f'Reservation for {self.dog.name} on {self.date_of_daycare}'
 
 
 # Testimonial Model
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Testimonial(models.Model):
@@ -74,20 +77,20 @@ class Testimonial(models.Model):
     )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateField(auto_now=True)
     content = models.TextField()
-    created_on = models.DateTimeField(default=timezone.now)
+    created_on = models.DateField()
     status = models.IntegerField(choices=STATUS, default=0)
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-created_on']
 
     def __str__(self):
         if self.author:
-            return f"Testimonial by {self.author.username}"
+            return f'Testimonial by {self.author.username}'
         else:
-            return "Anonymous Testimonial"
+            return 'Anonymous Testimonial'
 
 
 # Comment Model
@@ -97,9 +100,9 @@ class Comment(models.Model):
     testimonial = models.ForeignKey(
         Testimonial, related_name='comments', on_delete=models.CASCADE
     )
-    created_on = models.DateTimeField(default=timezone.now)
+    created_on = models.DateField()
     text = models.TextField()
     approved = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Comment by {self.user.username}"
+        return f'Comment by {self.user.username}'
