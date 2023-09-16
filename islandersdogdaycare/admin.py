f4c7b86f85e9e8b0e8adf2b0af19634459e296e4
jjanczynska <joaxjan@gmail.com>
2023-09-16 21:57:10 +0000

    Add custom function for approving testimonials by admin

--- islandersdogdaycare/admin.py
+++ islandersdogdaycare/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Owner, Dog, Reservation, Testimonial, Comment
 
+# SAdmin actions for approving testimonials
+
+
+def approve_testimonials(modeladmin, request, queryset):
+    queryset.update(approved=True)
+    approve_testimonials.short_description = "Approve selected testimonials"
+
 
 # Models registeration
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ['user', 'tel_no']
+
+
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'sreated_on', 'approved']
+    ordering = [approve_testimonials]
 
 
 admin.site.register(Owner, OwnerAdmin)
